383c6a6fdb68ce8759ef91b4a9c2ab121aad35ee
Alberto Carmona <btocarmona@gmail.com>
2025-05-21 19:55:23 -0300

    INICIO PROYECTO

---
+++ app/database/conecDB.py
@@ -0,0 +1,19 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class conectarDB:
+    def conectar():
+        try:
+            conn = mysql.connector.connect(
+                database=os.getenv("DB_NAME")
+                user=os.getenv("DB_USER")
+                password=os.getenv("DB_PASSWORD")
+                host=os.getenv("DB_HOST")
+                port=os.getenv("DB_PORT")
+            )
+            
+        except Exception as ex:
+          print(f"Error al conectar con la base de datos {ex}")
